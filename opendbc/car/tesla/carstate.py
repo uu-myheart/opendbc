c4858d952d08923365a659a17062affea9ca9a07
GaranPoe <gali3052@gmail.com>
2025-05-11 21:37:54 +0800

    Fix error

--- opendbc/car/tesla/carstate.py
+++ opendbc/car/tesla/carstate.py
@@ -56,6 +56,7 @@ class CarState(CarStateBase):
     # TODO: implement in safety
     eac_error_code = self.can_define.dv["EPAS3S_sysStatus"]["EPAS3S_eacErrorCode"].get(int(epas_status["EPAS3S_eacErrorCode"]), None)
     ret.steeringDisengage = self.hands_on_level >= 3 or (eac_status == "EAC_INHIBITED" and
+                                                         eac_error_code == "EAC_ERROR_HIGH_ANGLE_RATE_SAFETY")
 
     # Cruise state
     cruise_state = self.can_define.dv["DI_state"]["DI_cruiseState"].get(int(cp_party.vl["DI_state"]["DI_cruiseState"]), None)
